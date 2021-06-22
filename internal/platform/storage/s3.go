decdfe5b87797770ee0b5820039dbcea4c9dbbda
Daniela Osorio R <danosori@bancolombia.com.co>
2021-06-22 14:50:02 -0500

    Se agrega consulta con el cliente

--- internal/platform/storage/s3.go
+++ internal/platform/storage/s3.go
@@ -3,25 +3,22 @@ package s3
 import (
 	//"log"
 	"fmt"
-	"os"
+	//"os"
 	"nequi.com/poc-reading-files/internal/domain"
 	"github.com/aws/aws-sdk-go/aws"
     "github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	//"errors"
 )
 
 type S3Repository struct {
-	d *s3manager.Downloader
+	client *s3.S3
 }
 
 func NewS3Repository() *S3Repository {
-	sess, _ := session.NewSession(&aws.Config{Region: aws.String("us-east-1")},
-    )
-    downloader := s3manager.NewDownloader(sess)
+	svc := s3.New(session.New(),&aws.Config{Region: aws.String("us-east-1")})
 	return &S3Repository{
-		d : downloader,
+		client : svc,
 	}
 }
 
@@ -29,23 +26,13 @@ func NewS3Repository() *S3Repository {
 func (r *S3Repository) GetTransactions() (transaction.Transaction, error) {
 	transaction, error := transaction.NewTransaction(500,1, 2, 3, "debentura" )
 	
-	file, err := os.Create("myname")
-    if err != nil {
-        fmt.Println("Unable to open file %q, %v", "myname", err)
-    }
-
-    defer file.Close()
-	
-	numBytes, err := r.d.Download(file,
-        &s3.GetObjectInput{
-            Bucket: aws.String("nequi-s3-select-tmp"),
-            Key:    aws.String("resource/FINACLE_NEQUICARTERA_20200508_VENCIDOS.csv"),
-        })
+	obj, err := r.client.GetObject(&s3.GetObjectInput{
+        Bucket: aws.String("nequi-s3-select-tmp"),
+        Key:    aws.String("resource/FINACLE_NEQUICARTERA_20200508_VENCIDOS.csv"),
+    })
+	fmt.Println(obj)
     if err != nil {
         fmt.Println("pailas", err)
-    }
-
-    fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
-	
+    }	
 	return transaction, error
 }
